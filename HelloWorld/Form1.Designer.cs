7550683f569b1b7c950a7542325b2423a2dba41a
Nicholas Outram <noutram@PM6-0654.local>
2014-12-05 10:11:06 +0000

    Version 1.0

---
+++ HelloWorld/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace HelloWorld
+{
+    partial class MyForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.messageLabel = new System.Windows.Forms.Label();
+            this.revealMessageButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // messageLabel
+            // 
+            this.messageLabel.Font = new System.Drawing.Font("Palatino Linotype", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.messageLabel.Location = new System.Drawing.Point(43, 107);
+            this.messageLabel.Name = "messageLabel";
+            this.messageLabel.Size = new System.Drawing.Size(389, 98);
+            this.messageLabel.TabIndex = 0;
+            this.messageLabel.Text = "I have a message for you...";
+            this.messageLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // revealMessageButton
+            // 
+            this.revealMessageButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.revealMessageButton.Location = new System.Drawing.Point(125, 384);
+            this.revealMessageButton.Name = "revealMessageButton";
+            this.revealMessageButton.Size = new System.Drawing.Size(219, 47);
+            this.revealMessageButton.TabIndex = 1;
+            this.revealMessageButton.Text = "Tell me oh wise one";
+            this.revealMessageButton.UseVisualStyleBackColor = true;
+            this.revealMessageButton.Click += new System.EventHandler(this.revealMessageButton_Click);
+            this.revealMessageButton.MouseEnter += new System.EventHandler(this.revealMessageButton_MouseEnter);
+            // 
+            // MyForm
+            // 
+            this.AcceptButton = this.revealMessageButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 481);
+            this.Controls.Add(this.revealMessageButton);
+            this.Controls.Add(this.messageLabel);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "MyForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label messageLabel;
+        private System.Windows.Forms.Button revealMessageButton;
+
+    }
+}
+
---
+++ HelloWorld/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWorld
+{
+    public partial class MyForm : Form
+    {
+        ArrayList sayings;
+        Random rnd;
+        int index;
+
+        public MyForm()
+        {
+            InitializeComponent();
+            index = 0;
+            sayings = new ArrayList();
+            rnd = new Random();
+            sayings.Add("May the Force be with you always");
+            sayings.Add("Live Long and Prosper");
+            sayings.Add("To Infinity and Beyond");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void revealMessageButton_Click(object sender, EventArgs e)
+        {
+            String nextMessage = sayings[index] as String;
+            messageLabel.Text = nextMessage;
+            index = index + 1;
+            if (index > 2)
+            {
+                index = 0;
+            }
+        }
+
+        private void revealMessageButton_MouseEnter(object sender, EventArgs e)
+        {
+            int maxWidth  = this.Bounds.Width - revealMessageButton.Bounds.Width;
+            int maxHeight = this.Bounds.Height - revealMessageButton.Bounds.Height;
+            Point nextPosition = new Point(rnd.Next(maxWidth), rnd.Next(maxHeight));
+            revealMessageButton.Location = nextPosition;
+        }
+
+    }
+}
